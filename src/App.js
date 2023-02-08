edbcdef86b2ccae83886e5c9386a9ba0811a5d71
Priscila-Flores <priscilauchoaa@gmail.com>
2023-02-08 21:29:05 +0100

    Handle click on menu button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Form from './components/Form/Form';
+import Menu from './components/Menu/Menu';
 
 function App() {
   return (
     <div className='App'>
+      <Menu />
       <Form />
     </div>
   );
--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -3,10 +3,19 @@ import React, { useState } from 'react';
 import './Menu.css';
 export default function Menu() {
   const [navbarOpen, setNavbarOpen] = useState(false);
+  function handleClickOnMenu() {
+    setNavbarOpen(true);
+  }
   return (
     <nav className='navBar'>
-      <button>{navbarOpen ? 'Close' : 'Open'}</button>
-      <ul></ul>
+      <button onClick={handleClickOnMenu}>
+        {navbarOpen ? 'Close' : 'Open'}
+      </button>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
     </nav>
   );
 }
