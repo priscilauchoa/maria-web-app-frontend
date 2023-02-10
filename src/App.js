f575c6d29065b60959ea2d50266f2c344f418543
Priscila-Flores <priscilauchoaa@gmail.com>
2023-02-10 19:00:53 +0100

    add contact page to the app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Form from './components/Form/Form';
 import Burger from './components/Burger/Burger';
+import Contact from './components/Contact/Contact';
 
 function App() {
   return (
     <div className='App'>
       <Burger />
       <Form />
+      <Contact />
     </div>
   );
 }
