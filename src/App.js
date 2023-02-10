a1a2256a5a72e2629d2b1798a3e41dc745488ccc
Priscila-Flores <priscilauchoaa@gmail.com>
2023-02-10 19:11:34 +0100

    manage routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
+import { createBrowserHistory } from 'history';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Form from './components/Form/Form';
 import Burger from './components/Burger/Burger';
 import Contact from './components/Contact/Contact';
 
+const history = createBrowserHistory();
+
 function App() {
   return (
     <div className='App'>
       <Burger />
-      <Form />
-      <Contact />
+      <Router history={history}>
+        <Routes>
+          <Route exact path='/form' element={<Form to='/form' />} />
+        </Routes>
+        <Routes>
+          <Route exact path='/contact' element={<Contact to='/contact' />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
